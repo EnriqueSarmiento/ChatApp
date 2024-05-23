1be7d4b0f45ae032bb08db3f65dd2cf1943d68e6
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-05-23 17:11:16 -0400

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Enrique Sarmiento on 23/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
